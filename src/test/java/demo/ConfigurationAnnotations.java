92b7e8b9d22950a00b10282ca52dff4e2141c088
Zinaida <zmileshin@gmail.com>
2024-06-25 12:06:47 -0400

    hi

---
+++ src/test/java/demo/ConfigurationAnnotations.java
@@ -0,0 +1,71 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class ConfigurationAnnotations {
+
+WebDriver driver;
+@BeforeSuite
+public void beforeSuite(){
+    System.out.println("Before Suite: establishing db connection, creating a report object");
+}
+
+@AfterSuite
+public void afterSuite(){
+    System.out.println("After Suite: Closing db connection, generate a report");
+}
+    @BeforeMethod
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        System.out.println("Before Method");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("After Method");       driver.quit();
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("before class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After class");
+    }
+    @Test
+    public void testGoogle2() throws InterruptedException {
+
+        driver.get("https://www.google.com/");
+        String searchTerm = "tumbler";
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(500);
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+        System.out.println("Test Google2");
+
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void testGoogle3() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        String searchTerm = "screen protector";
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(500);
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+        System.out.println("TestGoogle3");
+
+    }
+}
---
+++ src/test/java/demo/TestNgDemo.java
@@ -0,0 +1,72 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestNgDemo {
+    WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void testGoogle() throws InterruptedException {
+
+        driver.get("https://www.google.com/");
+        String searchTerm = "coffee mug";
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(500);
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+
+
+
+
+    }
+
+    @Test
+    public void testGoogle2() throws InterruptedException {
+
+        driver.get("https://www.google.com/");
+        String searchTerm = "tumbler";
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(500);
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void testGoogle3() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        String searchTerm = "screen protector";
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(500);
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+        System.out.println("test test");
+    }
+
+
+}
